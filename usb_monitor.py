c821aac87c49d72b7a1dc925208c0aae7a0a9ccd
Barryblueice <barryblueice@outlook.com>
2025-07-25 00:10:09 +0800

    修改为hid设备

--- usb_monitor.py
+++ usb_monitor.py
@@ -16,24 +16,24 @@ class DeviceMonitorThread(QThread):
         while self.running:
             try:
                 dev = usb.core.find(idVendor=0x0D00, idProduct=0x0721)
-                is_mass_storage = False
+                is_hid_device = False
                 if dev:
                     try:
                         for cfg in dev:
                             for intf in cfg:
-                                if intf.bInterfaceClass == 0x08:
-                                    is_mass_storage = True
+                                if intf.bInterfaceClass == 0x03:
+                                    is_hid_device = True
                                     break
-                            if is_mass_storage:
+                            if is_hid_device:
                                 break
                     except usb.core.USBError:
                         pass
 
-                if is_mass_storage and not self.device_present:
+                if is_hid_device and not self.device_present:
                     self.device_present = True
                     self.device_event.emit(True)
 
-                elif not is_mass_storage and self.device_present:
+                elif not is_hid_device and self.device_present:
                     self.device_present = False
                     self.device_event.emit(False)
             except:
