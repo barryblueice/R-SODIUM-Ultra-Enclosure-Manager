1634663325b8ddf4be0c477961d19116a4a28824
Barryblueice <barryblueice@outlook.com>
2025-08-03 21:11:28 +0800

    修改UI

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import main_windows
 from interface import MainInterfaceController
 from PySide6 import QtWidgets
-from qt_material import apply_stylesheet
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
@@ -10,8 +9,6 @@ if __name__ == "__main__":
 
     controller = MainInterfaceController(window)
     controller.ui_initialized()
-
-    apply_stylesheet(app, theme='light_blue.xml')
 
     window.show()
     app.exec()
